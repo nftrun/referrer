acc774344285b8a90bb422edd61aec0f56c856cb
kilb <git@bll.io>
2022-07-28 15:34:48 +0800

    update

--- contracts/IRefStore.sol
+++ contracts/IRefStore.sol
@@ -6,6 +6,9 @@ interface IRefStore {
     function referrer(address from) external view returns (address);
     /// add referrer
     function addReferrer(address from, address to) external;
+    function addReferrerWithCheck(address to) external;
+    function open() external;
+    function close() external;
     /// referrer added
     event ReferrerAdded(address indexed to, address from);
 }
--- contracts/RefStore.sol
+++ contracts/RefStore.sol
@@ -5,20 +5,37 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 import "./IRefStore.sol";
 
 contract RefStore is IRefStore, AccessControl {
+    uint32 private _open = 1;
     bytes32 public constant APPEND_ROLE = keccak256("APPEND_ROLE");
     /// @dev referrer[user] => user
     mapping(address => address) public override referrer;
 
-    constructor() {
-        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _grantRole(APPEND_ROLE, msg.sender);
+    constructor(address admin) {
+        _grantRole(DEFAULT_ADMIN_ROLE, admin);
+        _grantRole(APPEND_ROLE, admin);
     }
 
     ///
     function addReferrer(address from, address to) external override {
-        require(referrer[msg.sender] == address(0), 'addReferrer: alreday add!');
-        require(hasRole(APPEND_ROLE, msg.sender), "Caller can not append referrer!");
+        require(referrer[from] == address(0), 'alreday add!');
+        require(hasRole(APPEND_ROLE, msg.sender), "access denied!");
         referrer[from] = to;
         emit ReferrerAdded(to, from);
+    }
+
+    function addReferrerWithCheck(address to) external override {
+        require(_open == 1, 'access denied!');
+        require(referrer[msg.sender] == address(0), 'alreday add!');
+        require(referrer[to] != address(0), 'invalid to!');
+        referrer[msg.sender] = to;
+        emit ReferrerAdded(to, msg.sender);
+    }
+
+    function open() external override onlyRole(DEFAULT_ADMIN_ROLE) {
+        _open = 1;
+    }
+
+    function close() external override onlyRole(DEFAULT_ADMIN_ROLE) {
+        _open = 0;
     }
 }
